578b5992e3f1143920617d98946997380fd757e6
Arjun-Go <arjun.g@thoughtworks.com>
2022-09-11 16:21:02 +0530

    Validate logged in user

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,64 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const config = require("config");
+
+//public
+router.post(
+  "/",
+  [
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Password already exists").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) console.log(err);
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 //private
 router.get("/", (req, res) => {
-  res.send("Get a user");
-});
-
-//public
-router.post("/", (req, res) => {
-  res.send("Log in a user");
+  res.send("Get logged in user");
 });
 
 module.exports = router;
