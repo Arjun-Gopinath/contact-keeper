9864c638d5dfdfc62988772dbdf5ecc26af5bc9e
Arjun-Go <arjun.g@thoughtworks.com>
2022-09-11 15:56:18 +0530

    Save user in db

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check/index");
+const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 //public
 router.post(
@@ -11,12 +12,28 @@ router.post(
     check("email", "Please enter a valid email").isEmail(),
     check("password", "The password is too short").isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send(req.body);
+
+    const { name, email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+
+    user = new User({
+      name,
+      email,
+      password,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+    res.send("User saved");
   }
 );
 
