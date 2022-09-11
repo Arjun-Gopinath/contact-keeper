2dd1ebd4965ef7bf0bf5c920d03f81620950c764
Arjun-Go <arjun.g@thoughtworks.com>
2022-09-11 15:35:30 +0530

    Add user model and validations

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check/index");
+const User = require("../models/User");
 
 //public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please add name").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "The password is too short").isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send(req.body);
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 connectDB();
 
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Hello World"));
 
 app.use("/api/users", require("./routes/users"));
