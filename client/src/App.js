a0a719757f6787aa553e56baacbf2d980af2e03a
Arjun-Go <arjun.g@thoughtworks.com>
2022-09-20 12:26:52 +0530

    Add. register, not found pages and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import styles from "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Switch } from "react-router";
+import { BrowserRouter, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
     <div className={styles.App}>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/register" element={<Register />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
-      <Login />
     </div>
   );
 };
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,49 @@
 import React from "react";
+import styles from "./Register.module.css";
+import Logo from "../assets/Logo.png";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.loginWindow}>
+        <input
+          className={styles.input}
+          type="text"
+          name="Name"
+          placeholder="Name"
+        />
+        <input
+          className={styles.input}
+          type="text"
+          name="Phone"
+          placeholder="Phone Number"
+        />
+        <input
+          className={styles.input}
+          type="text"
+          name="Email"
+          placeholder="Email"
+        />
+        <input
+          className={styles.input}
+          type="text"
+          name="Password"
+          placeholder="Password"
+        />
+        <input
+          className={styles.submit}
+          type="submit"
+          value="Register"
+          placeholder="Register"
+        />
+        <a href="/login">Already a user? Login</a>
+      </div>
+      <div className={styles.logo}>
+        <img src={Logo} />
+        <h1>Contact Keeper</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
